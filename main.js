00441897fa950ed8da4ff7141012c4e35d5bd3cd
najmus028 <najmus028@gmail.com>
2020-08-18 11:27:02 -0400

    final commit

---
+++ main.js
@@ -0,0 +1,77 @@
+document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
+
+function submitIssue(e) {
+  const getInputValue = id => document.getElementById(id).value;
+  const description = getInputValue('issueDescription');
+  const severity = getInputValue('issueSeverity');
+  const assignedTo = getInputValue('issueAssignedTo');
+  const id = Math.floor(Math.random()*100000000) + '';
+  const status = 'Open';
+
+  const issue = { id, description, severity, assignedTo, status };
+  let issues = [];
+  if (localStorage.getItem('issues')){
+    issues = JSON.parse(localStorage.getItem('issues'));
+  }
+  issues.push(issue);
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  document.getElementById('issueInputForm').reset();
+  fetchIssues();
+  e.preventDefault();
+}
+
+const setStatusClosed = id => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const currentIssue = issues.find(issue => issue.id == id);
+  currentIssue.status = 'Closed';
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues();
+}
+
+const deleteIssue = id => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const remainingIssues = issues.filter(issue => issue.id != id);
+  // remainingIssues.splice(id, 1);
+  // console.log(remainingIssues);
+  // console.log(id);
+  localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues();
+}
+
+const fetchIssues = () => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const issuesList = document.getElementById('issuesList');
+  issuesList.innerHTML = '';
+
+  let count = 0;
+  for (var i = 0; i < issues.length; i++) {
+    const {id, description, severity, assignedTo, status} = issues[i];
+    if (status == 'Closed'){
+      count++;
+      issuesList.innerHTML +=   `<div class="well">
+                              <h6>Issue ID: ${id.strike()} </h6>
+                              <p><span class="label label-info"> ${status} </span></p>
+                              <h3> ${description.strike()} </h3>
+                              <p><span class="glyphicon glyphicon-time"></span> ${severity.strike()}</p>
+                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo.strike()}</p>
+                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              </div>`;
+    }
+    else{
+      issuesList.innerHTML +=   `<div class="well">
+                              <h6>Issue ID: ${id} </h6>
+                              <p><span class="label label-info"> ${status} </span></p>
+                              <h3> ${description} </h3>
+                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              </div>`;
+    }
+    document.getElementById('totalIssue').innerText = "Total Issues: "+issues.length;
+    document.getElementById('closedIssue').innerText = count;
+
+  }
+}
